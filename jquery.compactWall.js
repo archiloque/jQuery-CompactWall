34383f348fb01fc182fc776d50d0447764c31ab1
archiloque <archiloque@archiloque.net>
2012-04-22 17:19:03 +0200

    transformed methods into functions + excludes slots too narrow

--- jquery.compactWall.js
+++ jquery.compactWall.js
@@ -10,33 +10,75 @@
 (function ($) {
     $.fn.compactWall = function (blocks) {
 
-        /**
-         * Represents a position of block.
-         * @param containerWith the with of the container.
-         * @param positionedBlocks the already positioned blocks.
-         * @param slots the list of slots.
-         * @param height the current position's height.
-         */
-        var BlocksPosition = function (positionedBlocks, slots, height, containerWith) {
-            this.positionedBlocks = positionedBlocks;
-            this.height = height;
-            this.slots = slots;
+        // slots represents position where blocks can be placed
+        // they are sorted from left to right
 
-            // slots represents position where blocks can be placed
-            // they are sorted from left to right
+        function sameHeightSameWidth(slots, slotIndex) {
+            return slots.slice(0, slotIndex)
+                .concat(slots.slice(slotIndex + 1));
+        }
 
-            this.sameHeightSameWidth = function (slotIndex) {
+        function sameHeightNarrower(slots, slotIndex, slot, block, minBlockWidth) {
+            if ((slots.length > (slotIndex + 1)) &&
+                (slots[slotIndex + 1].left == (slot.left + block.width)) &&
+                (slots[slotIndex + 1].availableWidth == (slot.availableWidth - block.width))) {
+                // wannabe new position is aligned with the next slot
+                // this design could have been reached by another organisation
+                // but visually it is interesting
                 return slots.slice(0, slotIndex)
-                    .concat(slots.slice(slotIndex + 1));
-            };
+                    .concat([
+                    {
+                        top: slots[slotIndex + 1].top,
+                        left: slots[slotIndex + 1].left,
+                        availableWidth: slots[slotIndex + 1].availableWidth,
+                        availableHeight: (slots[slotIndex + 1].availableHeight + block.height)
+                    }
+                ]).concat(slots.slice(slotIndex + 2));
+            } else {
+                var availableWidth = slot.availableWidth - block.width;
+                if (availableWidth >= minBlockWidth) {
+                    return slots.slice(0, slotIndex)
+                        .concat([
+                        {
+                            top: slot.top,
+                            left: slot.left + block.width,
+                            availableWidth: availableWidth,
+                            availableHeight: block.height
+                        }
+                    ]).concat(slots.slice(slotIndex + 1));
+                } else {
+                    return slots.slice(0, slotIndex).concat(slots.slice(slotIndex + 1));
+                }
+            }
+        }
 
-            this.sameHeightNarrower = function (slotIndex, slot, block) {
-                if ((slots.length > (slotIndex + 1)) &&
-                    (slots[slotIndex + 1].left == (slot.left + block.width)) &&
-                    (slots[slotIndex + 1].availableWidth == (slot.availableWidth - block.width))) {
-                    // wannabe new position is aligned with the next slot
-                    // this design could have been reached by another organisation
-                    // but visually it is interesting
+        function smallerNarrower(slots, slotIndex, slot, block, minBlockWidth) {
+            if ((slots.length > (slotIndex + 1)) &&
+                (slots[slotIndex + 1].left == (slot.left + block.width)) &&
+                (slots[slotIndex + 1].availableWidth == (slot.availableWidth - block.width))) {
+                // wannabe new slot is aligned with the next slot
+                // this design could have been reached by another organisation
+                // but visually it is interesting
+                if (slotIndex == 0) {
+                    // the current slot is the first one
+                    // move it to the bottom and increase height of next one
+                    return [
+                        {
+                            top: slot.top + block.height,
+                            left: 0,
+                            availableWidth: slot.availableWidth,
+                            availableHeight: Number.Infinity
+                        },
+                        {
+                            top: slots[slotIndex + 1].top,
+                            left: slots[slotIndex + 1].left,
+                            availableWidth: slots[slotIndex + 1].availableWidth,
+                            availableHeight: (slots[slotIndex + 1].availableHeight + block.height)
+                        }
+                    ].concat(slots.slice(slotIndex + 2));
+                } else {
+                    // current slot is not the first one
+                    // increase the size of next one
                     return slots.slice(0, slotIndex)
                         .concat([
                         {
@@ -46,69 +88,10 @@
                             availableHeight: (slots[slotIndex + 1].availableHeight + block.height)
                         }
                     ]).concat(slots.slice(slotIndex + 2));
-                } else {
-                    return slots.slice(0, slotIndex)
-                        .concat([
-                        {
-                            top: slot.top,
-                            left: slot.left + block.width,
-                            availableWidth: (slot.availableWidth - block.width),
-                            availableHeight: block.height
-                        }
-                    ]).concat(slots.slice(slotIndex + 1));
                 }
-            };
-
-            this.smallerSameWidth = function (slotIndex, slot, block) {
-                return slots.slice(0, slotIndex)
-                    .concat([
-                    {
-                        top: slot.top + block.height,
-                        left: slot.left,
-                        availableWidth: slot.availableWidth,
-                        availableHeight: (slot.availableHeight - block.height)
-                    }
-                ]).concat(slots.slice(slotIndex + 1));
-            };
-
-            this.smallerNarrower = function (slotIndex, slot, block) {
-                if ((slots.length > (slotIndex + 1)) &&
-                    (slots[slotIndex + 1].left == (slot.left + block.width)) &&
-                    (slots[slotIndex + 1].availableWidth == (slot.availableWidth - block.width))) {
-                    // wannabe new slot is aligned with the next slot
-                    // this design could have been reached by another organisation
-                    // but visually it is interesting
-                    if (slotIndex == 0) {
-                        // the current slot is the first one
-                        // move it to the bottom and increase height of next one
-                        return [
-                            {
-                                top: slot.top + block.height,
-                                left: 0,
-                                availableWidth: slot.availableWidth,
-                                availableHeight: Number.Infinity
-                            },
-                            {
-                                top: slots[slotIndex + 1].top,
-                                left: slots[slotIndex + 1].left,
-                                availableWidth: slots[slotIndex + 1].availableWidth,
-                                availableHeight: (slots[slotIndex + 1].availableHeight + block.height)
-                            }
-                        ].concat(slots.slice(slotIndex + 2));
-                    } else {
-                        // current slot is not the first one
-                        // increase the size of next one
-                        return slots.slice(0, slotIndex)
-                            .concat([
-                            {
-                                top: slots[slotIndex + 1].top,
-                                left: slots[slotIndex + 1].left,
-                                availableWidth: slots[slotIndex + 1].availableWidth,
-                                availableHeight: (slots[slotIndex + 1].availableHeight + block.height)
-                            }
-                        ]).concat(slots.slice(slotIndex + 2));
-                    }
-                } else {
+            } else {
+                var availableWidth = (slot.availableWidth - block.width);
+                if (availableWidth >= minBlockWidth) {
                     return slots.slice(0, slotIndex)
                         .concat([
                         {
@@ -120,12 +103,47 @@
                         {
                             top: slot.top,
                             left: slot.left + block.width,
-                            availableWidth: (slot.availableWidth - block.width),
+                            availableWidth: availableWidth,
                             availableHeight: block.height
                         }
                     ]).concat(slots.slice(slotIndex + 1));
+                } else {
+                    return slots.slice(0, slotIndex)
+                        .concat([
+                        {
+                            top: slot.top + block.height,
+                            left: slot.left,
+                            availableWidth: slot.availableWidth,
+                            availableHeight: (slot.availableHeight - block.height)
+                        }
+                    ]).concat(slots.slice(slotIndex + 1));
                 }
-            };
+            }
+        }
+
+        function smallerSameWidth(slots, slotIndex, slot, block) {
+            return slots.slice(0, slotIndex)
+                .concat([
+                {
+                    top: slot.top + block.height,
+                    left: slot.left,
+                    availableWidth: slot.availableWidth,
+                    availableHeight: (slot.availableHeight - block.height)
+                }
+            ]).concat(slots.slice(slotIndex + 1));
+        }
+
+        /**
+         * Represents a position of block.
+         * @param containerWith the with of the container.
+         * @param positionedBlocks the already positioned blocks.
+         * @param slots the list of slots.
+         * @param height the current position's height.
+         */
+        var BlocksPosition = function (positionedBlocks, slots, height, containerWith, minBlockWidth) {
+            this.positionedBlocks = positionedBlocks;
+            this.height = height;
+            this.slots = slots;
 
             /**
              * Add a block to this position
@@ -155,27 +173,28 @@
                                 // the block has the same height than the slot
                                 if (slot.availableWidth == block.width) {
                                     // same height and same width: we kill the slot
-                                    newSlots = this.sameHeightSameWidth(slotIndex);
+                                    newSlots = sameHeightSameWidth(slots, slotIndex);
                                 } else {
                                     // same height but narrower : we move the slot to the right
-                                    newSlots = this.sameHeightNarrower(slotIndex, slot, block);
+                                    newSlots = sameHeightNarrower(slots, slotIndex, slot, block, minBlockWidth);
                                 }
                             } else {
                                 // the blocks is smaller
                                 if (block.width == slot.availableWidth) {
                                     // same width but smaller : we move the slot to the bottom
-                                    newSlots = this.smallerSameWidth(slotIndex, slot, block);
+                                    newSlots = smallerSameWidth(slots, slotIndex, slot, block);
                                 } else {
                                     // smaller width and smaller height: we move the slot to the bottom
                                     // and add another slot for the upper right corner of the block
-                                    newSlots = this.smallerNarrower(slotIndex, slot, block);
+                                    newSlots = smallerNarrower(slots, slotIndex, slot, block, minBlockWidth);
                                 }
                             }
                             result.push(new BlocksPosition(
                                 positionedBlocks.concat([positionedBlock]),
                                 newSlots,
                                 Math.max(height, slot.top + block.height),
-                                containerWith));
+                                containerWith,
+                                minBlockWidth));
                         }
                     }
                     return result;
@@ -183,21 +202,21 @@
             }
         };
 
-        var bestFit = function (blocksList, containerWidth) {
+        function bestFit(blocksList, containerWidth, minBlockWidth) {
             var initialSlot = {
                 top: 0,
                 left: 0,
                 availableWidth: containerWidth,
                 availableHeight: Number.POSITIVE_INFINITY
             };
-            var position = new BlocksPosition([], [initialSlot], 0, containerWidth);
+            var position = new BlocksPosition([], [initialSlot], 0, containerWidth, minBlockWidth);
             return bestFitInternal(blocksList, position, Number.POSITIVE_INFINITY);
-        };
+        }
 
         /**
          * Internal version of bestFit.
          */
-        var bestFitInternal = function (remainingBlocks, position, maxHeight) {
+        function bestFitInternal(remainingBlocks, position, maxHeight) {
             if (remainingBlocks.length == 1) {
                 return bestFitLastBlock(remainingBlocks[0], position, maxHeight);
             } else {
@@ -213,12 +232,12 @@
                 }
                 return bestPosition;
             }
-        };
+        }
 
         /**
          * Find the best fitting position when adding the last block.
          */
-        var bestFitLastBlock = function (remainingBlock, position, maxHeight) {
+        function bestFitLastBlock(remainingBlock, position, maxHeight) {
             var bestPosition = null;
             var minHeight = maxHeight;
             var positions = position.addBlock(remainingBlock, minHeight);
@@ -230,12 +249,12 @@
                 }
             }
             return bestPosition;
-        };
+        }
 
         /**
          * Find the best fitting position when adding a block to a position
          */
-        var nextFitNextBlock = function (remainingBlocks, block, index, position, maxHeight) {
+        function nextFitNextBlock(remainingBlocks, block, index, position, maxHeight) {
             var bestPosition = null;
             var minHeight = maxHeight;
             var positions = position.addBlock(block, minHeight);
@@ -253,21 +272,27 @@
                 }
             }
             return bestPosition;
-        };
+        }
 
         return this.each(function (i, container) {
             // the block lists
             // all have an height, a width, and a block
+            var minBlockWidth = Number.POSITIVE_INFINITY;
+
             var blockList = $.map(blocks, function (block) {
                 var blockJ = $(block);
+                var width = blockJ.outerWidth(true);
+                if (width < minBlockWidth) {
+                    minBlockWidth = width
+                }
                 return {
-                    width: blockJ.outerWidth(true),
+                    width: width,
                     height: blockJ.outerHeight(true),
                     block: block
                 };
             });
 
-            var position = bestFit(blockList, $(container).innerWidth());
+            var position = bestFit(blockList, $(container).innerWidth(), minBlockWidth);
             if (position) {
                 $.each(position.positionedBlocks, function (index, positionedBlock) {
                     $(positionedBlock.block.block).
